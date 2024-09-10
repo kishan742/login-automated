3d6c605e661004856c58fdd94f17a4efd473f617
kishan742 <kishankumar80504@gmail.com>
2024-09-10 13:26:57 +0530

    Add files via upload

---
+++ LoginPageUnSuccess.java
@@ -0,0 +1,33 @@
+package seleniumproj;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPageUnSuccess {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","/Users/HP/Downloads/chromedriver-win64/chromedriver-win64/chromedriver.exe/");
+        WebDriver driver = new ChromeDriver();
+        try {
+        	driver.get("https://app.germanyiscalling.com/common/login/");
+        	WebElement emailField = driver.findElement(By.id("Email"));
+        	WebElement passwordField = driver.findElement(By.id("password"));
+        	WebElement loginButton = driver.findElement(By.id("Log In"));
+        	emailField.sendKeys("kkumar12345@gmail.com");
+        	passwordField.sendKeys("kkumar145@");
+        	loginButton.click();
+        	WebElement errorMessage = driver.findElement(By.id("Please enter a correct username and password. Note that both fields may be case-sensitive."));
+        	if(errorMessage.isDisplayed()) {
+        		System.out.println("Login Unsuccessfull. Please enter a correct username and password. Note that both fields may be case-sensitive.");       		
+        	} else {
+        		System.out.println("Error message not displayed for invalid login.");        		
+        	}        	
+        } finally {
+        	driver.quit();      	
+        }       
+	}
+
+}
+
